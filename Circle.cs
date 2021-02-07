4895d966b48e1cf8f9b79384ba920b90a767236f
nmausbach <56225438+nmausbach@users.noreply.github.com>
2021-02-07 16:50:59 -0600

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MausbachProject2
+{
+    class Circle
+    {
+        public static void Main(string[] args)
+        {
+            MausbachHeading.getHeading("Assignment 2"); //open heading
+
+            //begin of circle assignment
+            Console.WriteLine("Enter the radius of a circle ");
+            int radius = int.Parse(Console.ReadLine());
+
+            //test formulas
+            /*double diameter = 2 * radius;
+            double circumference = 2 * 3.14159 * radius;
+            double area = 3.14159 * radius * radius;
+            */
+
+            double pi = Math.PI;
+
+            //display results
+            Console.WriteLine();
+            Console.WriteLine($"Diameter {2 * radius}\nCircumference {2 * pi * radius}\nArea {pi * radius * radius}");
+
+
+            MausbachHeading.getClosing(); //open closing
+        }
+    }
+}
